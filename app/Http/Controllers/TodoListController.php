8734141e33789fd375dd3a9e5c687c608a7cddcd
Maleeha Kaikaus <77163845+mkaikaus@users.noreply.github.com>
2023-09-04 10:22:47 +0600

    Update and delete

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -29,4 +29,17 @@ class TodoListController extends Controller
         $request->validate(['name'=>['required']]);
         return TodoList::create(['name' => $request->name]);
     }
+
+    public function destroy(TodoList $list)
+    {
+        $list->delete();
+        return response('', Response::HTTP_NO_CONTENT);
+    }
+
+    public function update(Request $request, TodoList $list)
+    {
+        $request->validate(['name' => ['required']]);
+        $list->update($request->all());
+        return response($list);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,7 @@ Route::get('todo-list',[TodoListController::class,'index'])->name('todo-list.ind
 Route::get('todo-list/{todolist}',[TodoListController::class,'show'])->name('todo-list.show');
 
 Route::post('todo-list',[TodoListController::class,'store'])->name('todo-list.store');
+
+Route::delete('todo-list/{list}',[TodoListController::class,'destroy'])->name('todo-list.destroy');
+
+Route::patch('todo-list/{list}',[TodoListController::class,'update'])->name('todo-list.update');
